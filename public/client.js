d042da6f85f47ea58d1e559baed085c3addcc128
Patrick Soares de souza silva <patrick.soares@iholdbank.digital>
2023-09-16 09:31:44 -0300

    console log

--- public/client.js
+++ public/client.js
@@ -25,46 +25,11 @@ export const _fetch = async (path, payload = '') => {
   };
   
   export const registerCredential = async () => {
-  const opts = {
-    attestation: 'none',
-    authenticatorSelection: {
-      authenticatorAttachment: 'platform',
-      userVerification: 'required',
-      requireResidentKey: false
-    }
-  };
-
   const options = await _fetch('/registerBio', opts);
   const encoder = new TextEncoder();
   let uint8Array = new Uint8Array(options.challenge);
-
-//     options.user.id = base64url.decode(options.user.id);
-//   options.challenge = base64url.decode(options.challenge);
   options.user.id = encoder.encode(options.user.id);
   options.challenge = uint8Array
-
-  if (options.excludeCredentials) {
-    for (let cred of options.excludeCredentials) {
-    //   cred.id = base64url.decode(cred.id);
-      cred.id = cred.id;
-    }
-  }
-  const publicKeyCredentialCreationOptions = {
-    challenge: options.challenge,
-    rp: {
-      name: "eaglesoftwaremarcos",
-      id: "eaglesoftware.com.br",
-    },
-    user: {
-      id: new Uint8Array(16), // Identificador único do usuário
-      name: "Marcos Paulo", // Nome do usuário
-      displayName: "Machadinho", // Nome a ser exibido
-    },
-    pubKeyCredParams: [{ alg: -7, type: "public-key" }],
-    
-    timeout: 60000,
-  };
-  console.log(window.location.hostname)
     const cred = await navigator.credentials.create({
     publicKey: options,
   });
@@ -72,7 +37,7 @@ export const _fetch = async (path, payload = '') => {
   credential.id = cred.id;
   credential.rawId = cred.rawId;
   credential.type = cred.type;
-  
+    console.log(credential);
   if (cred.response) {
     const clientDataJSON =
       cred.response.clientDataJSON;
