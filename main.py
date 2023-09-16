563ebac8e1699d3aa4505ff6fd9c5faac63365f0
Patrick Soares de souza silva <patrick.soares@iholdbank.digital>
2023-09-16 08:49:35 -0300

    heroku_id

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ def your_endpoint_name(body: dict, request: Request):
         id = uuid_v4
         options  : PublicKeyCredentialCreationOptions= generate_registration_options(
             rp_name= RP_NAME,
-            rp_id= request.base_url.hostname,
+            rp_id= os.environ.get("RP_ID", request.base_url.hostname),
             user_id= user["id"],
             user_name= user["username"],
             challenge=bytearray(32),
